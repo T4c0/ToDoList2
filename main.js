f8e6c4677d3cf35c358be0392cfda2f6f95fe385
Julian Vazquez <julianv483@gmail.com>
2022-04-07 11:38:36 -0500

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
-    const list_el = document.querySelector("#tasks")
+    const list_el = document.querySelector("#tasks");
+
+    const LOCAL_STORAGE_LIST_KEY = 'lists.task';        //***INTIGRATE LOCAL STORAGE FOR LIST MEMORY**//
+    let task = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || "";
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(task));
 
     
     
